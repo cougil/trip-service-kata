923da6d97903ed1bfebce09d34a0c4a519027ade
Nacho Cougil <ncougil@gmail.com>
2017-05-10 12:17:08 +0100

    Returning an empty list when users are not friends

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -4,19 +4,37 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class TripServiceShould {
 
     @Test(expected = UserNotLoggedInException.class)
     public void thrown_an_exception_if_user_not_logged_in() {
-        TripService tripService = new TripServiceTest();
         User user = new User();
-        tripService.getTripsByUser(user);
+        User loggedUser = null;
+        TripService tripServiceTest = new TripServiceTest(loggedUser);
+        tripServiceTest.getTripsByUser(user);
+    }
+
+    @Test
+    public void return_empty_list_when_users_are_not_friends() {
+        User user = new User();
+        TripService tripServiceTest = new TripServiceTest(user);
+        User friend = new User();
+        assertThat(tripServiceTest.getTripsByUser(user).size(),is(0));
     }
 
     private class TripServiceTest extends TripService {
+        private final User loggedUser;
+
+        public TripServiceTest(User loggedUser) {
+            this.loggedUser = loggedUser;
+        }
+
         @Override
         protected User getLoggedUser() {
-            return null;
+            return loggedUser;
         }
     }
 }
